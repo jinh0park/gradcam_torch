23754f64fe9fac8634b360055144009bbef095c2
jinh0park <jinh0park@naver.com>
2019-02-04 00:01:45 +0900

    modified model architecture

--- model.py
+++ model.py
@@ -11,9 +11,15 @@ class ZeroNet(nn.Module):
             nn.Conv2d(3,64,3,stride=1,padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2,stride=2),
+            nn.Conv2d(64,64,3,stride=1,padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3,stride=3),
 
             nn.Conv2d(64,128,3,stride=1,padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128,3,stride=1,padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2,stride=2),
@@ -27,20 +33,15 @@ class ZeroNet(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2,stride=2),
-
-            nn.Conv2d(256,256,3,stride=1,padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-           
         )
         self.classifier = nn.Sequential(
-            nn.Linear(6*6*256, 1024),
+            nn.Linear(4*4*256, 512),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(1024, 1024),
+            nn.Linear(512, 512),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(1024, num_classes),
+            nn.Linear(512, num_classes),
         )
 
     def forward(self,x):
